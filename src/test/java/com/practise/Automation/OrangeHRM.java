0a8de68bc3dd8dde8f96b14cc6b1a199ac3e298f
Ahsan Habib <smahsanhabib1999@gmail.com>
2025-01-09 21:39:31 +0600

    first commit

---
+++ src/test/java/com/practise/Automation/OrangeHRM.java
@@ -0,0 +1,69 @@
+package com.practise.Automation;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class OrangeHRM {
+
+    WebDriver driver;
+
+    @BeforeSuite
+    public void startChromeBrowser() {
+        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void testOrangeHRM() throws Exception {
+
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        Thread.sleep(15000);
+
+        //Login
+        WebElement name = driver.findElement(By.name("username"));
+        name.sendKeys("Admin");
+        Thread.sleep(3000);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+        Thread.sleep(3000);
+
+        WebElement login = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[3]/button[1]"));
+        login.click();
+        Thread.sleep(3000);
+
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList");
+        Thread.sleep(3000);
+
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee");
+        Thread.sleep(3000);
+
+        WebElement firstName = driver.findElement(By.name("firstName"));
+        firstName.sendKeys("Ahsan");
+        Thread.sleep(5000);
+
+        WebElement lastName = driver.findElement(By.name("lastName"));
+        lastName.sendKeys("Habib");
+        Thread.sleep(5000);
+
+        WebElement save = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[2]/button[2]"));
+        save.click();
+        Thread.sleep(15000);
+
+
+    }
+
+    @AfterSuite
+
+    public void closeChromeBrowser() {
+        driver.quit();
+
+    }
+
+}
+
