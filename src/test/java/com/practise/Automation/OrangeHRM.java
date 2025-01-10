48f2f38ceb8a7d8c6d90525a35983f7a348144a0
Ahsan Habib <smahsanhabib1999@gmail.com>
2025-01-10 23:37:11 +0600

    first commit

--- src/test/java/com/practise/Automation/OrangeHRM.java
+++ src/test/java/com/practise/Automation/OrangeHRM.java
@@ -24,33 +24,38 @@ public class OrangeHRM {
         driver.get("https://opensource-demo.orangehrmlive.com/");
         Thread.sleep(15000);
 
-        //Login
+        //Login username
         WebElement name = driver.findElement(By.name("username"));
         name.sendKeys("Admin");
         Thread.sleep(3000);
-
+        //Login Password
         WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("admin123");
         Thread.sleep(3000);
 
+        //Login Button
         WebElement login = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[3]/button[1]"));
         login.click();
         Thread.sleep(3000);
 
+        //Navigation to view Employee
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList");
         Thread.sleep(3000);
-
+        //Navigate to PIM
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee");
         Thread.sleep(3000);
 
+        //For First Name
         WebElement firstName = driver.findElement(By.name("firstName"));
         firstName.sendKeys("Ahsan");
         Thread.sleep(5000);
 
+        //For Sec ond Name
         WebElement lastName = driver.findElement(By.name("lastName"));
         lastName.sendKeys("Habib");
         Thread.sleep(5000);
 
+         // For Save Button
         WebElement save = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[2]/button[2]"));
         save.click();
         Thread.sleep(15000);
